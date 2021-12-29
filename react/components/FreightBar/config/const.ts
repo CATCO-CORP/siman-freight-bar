5d2c898725be7fbea8274050ae9724885cf3f2de
SavioChermont <saviochwt@gmail.com>
2021-12-29 16:38:02 -0300

    fix: set app to guatemala config

--- react/components/FreightBar/config/const.ts
+++ react/components/FreightBar/config/const.ts
@@ -1,2 +1,2 @@
 
-export const PROMOTION_ID = "18f3818f-5475-4c76-b7c9-7d67801adb75"; // Get PromotionID using this API: https://developers.vtex.com/vtex-rest-api/reference/promotions#getallbenefits
+export const PROMOTION_ID = "c6bf4e00-63b4-4ae9-8157-00bade31e3e7"; // Get PromotionID using this API: https://developers.vtex.com/vtex-rest-api/reference/promotions#getallbenefits
